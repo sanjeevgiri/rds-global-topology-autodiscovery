0743d5526bdd389f0d15dc3a042a80b844208b91
Sanjeev Giri <sgirism@gmail.com>
2023-04-07 14:41:11 -0400

    readme

---
+++ src/test/java/com/rdsglobal/topology/autodiscovery/AutodiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdsglobal.topology.autodiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutodiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
